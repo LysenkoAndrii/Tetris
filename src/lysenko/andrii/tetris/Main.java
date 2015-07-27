16bf424ad6c8b7de85ab52583b66c719b529db2a
LysenkoAndrii <senkoily@gmail.com>
2015-07-27 17:33:43 +0300

    Rejected commit: Default

--- src/lysenko/andrii/tetris/Main.java
+++ src/lysenko/andrii/tetris/Main.java
@@ -3,15 +3,13 @@ package lysenko.andrii.tetris;
 import lysenko.andrii.tetris.mvc.Controller;
 
 /**
+ * Main class.
  * Created by admin on 27.07.2015.
  */
 public class Main {
     public static void main(String[] args) {
-        boolean graphicalView;
-        if (args.length == 1)
-            graphicalView = args[0].equals("-graphical");
-        else
-            graphicalView = false;
-        Controller controller = new Controller(graphicalView);
+        boolean graphicalView = (args.length == 1) &&
+                args[0].equals("-graphical");
+        new Controller(graphicalView);
     }
 }
